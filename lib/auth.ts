75f4d5f162463a1d9983ad245945e673028bd1d1
shakibul-islam-dev <ishakibul704@gmail.com>
2026-09-19 16:33:34 +0600

    Adding Full Package of shadcn ui,better auth,zod,hook-form ,axois

---
+++ lib/auth.ts
@@ -0,0 +1,30 @@
+import { betterAuth } from "better-auth";
+import { emailOTP } from "better-auth/plugins";
+export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+    facebook: {
+      clientId: process.env.FACEBOOK_CLIENT_ID as string,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
+    },
+  },
+  plugins: [
+    emailOTP({
+      async sendVerificationOTP({ email, otp, type }) {
+        if (type === "sign-in") {
+          // Send the OTP for sign in
+        } else if (type === "email-verification") {
+          // Send the OTP for email verification
+        } else {
+          // Send the OTP for password reset
+        }
+      },
+    }),
+  ],
+});
