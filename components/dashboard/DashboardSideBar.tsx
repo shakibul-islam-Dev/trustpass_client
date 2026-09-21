78d403daff0a03fcfbfb339dc04ab613b3d9f536
AritraApon <aritromazumdar8@gmail.com>
2026-09-21 18:58:35 +0600

    git cmt

--- components/dashboard/DashboardSideBar.tsx
+++ components/dashboard/DashboardSideBar.tsx
@@ -103,6 +103,7 @@ export default function DashboardSideBar({ userRole }: SidebarProps) {
 
           {/* log out button  */}
           {/* github check  */}
+            {/* git check ddddddd */}
       {/* 3. Footer */}
       <SidebarFooter className="p-3 border-t border-border/40">
         <div className="flex items-center justify-between p-2.5 rounded-xl bg-muted/50 hover:bg-muted transition-colors">
