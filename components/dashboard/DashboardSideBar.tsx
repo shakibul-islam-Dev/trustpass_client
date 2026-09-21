42017ff18ae7d8844b7eca2415dc5ce888718452
AritraApon <aritromazumdar8@gmail.com>
2026-09-21 18:34:28 +0600

    fixed

--- components/dashboard/DashboardSideBar.tsx
+++ components/dashboard/DashboardSideBar.tsx
@@ -101,6 +101,7 @@ export default function DashboardSideBar({ userRole }: SidebarProps) {
         </SidebarGroup>
       </SidebarContent>
 
+          {/* log out button  */}
       {/* 3. Footer */}
       <SidebarFooter className="p-3 border-t border-border/40">
         <div className="flex items-center justify-between p-2.5 rounded-xl bg-muted/50 hover:bg-muted transition-colors">
