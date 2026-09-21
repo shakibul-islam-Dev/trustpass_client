17de599637720c8a1364d6dfa14e2d431c62d838
shakibul-islam-dev <ishakibul704@gmail.com>
2026-09-21 10:31:04 +0600

    Delet Problem

--- app/dashboard/layout.tsx
+++
@@ -1,6 +0,0 @@
-import GETFULLDATA from "@/lib/data";
-
-export default function DashbaordRootLayout({ children }: LayoutProps<"/">) {
-   console.log(GETFULLDATA)
-  return <h1>`hfoeifihoef{user.name}`</h1>;
-}
